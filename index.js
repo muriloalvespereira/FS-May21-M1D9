e29295454a7f863510062d13e5576aba63031e3f
Murilo Alves <murilo.udi@gmail.com>
2021-05-27 18:52:18 +0200

    number is highlighted on the bingo board

--- index.js
+++ index.js
@@ -2,26 +2,30 @@
 window.onload = function(){
 
     displayEntireMonth()
-    const randomNum = function (newRandom) {
-        let changeNum = document.getElementsByTagName("h1")[0];
-        changeNum.innerText =newRandom;
-      };
-      const btn1= document.querySelector("#random-num")
-      btn1.addEventListener('click', function(){
-        randomNum (Math.round(Math.random() * 76));
-           })
 }
-    
+
 
 const displayEntireMonth = function(){
     
     let numbersContainer = document.getElementById("numbers-container")
     numbersContainer.innerHTML = ""
 
-    for (let i = 1; i <= 76; i++){
+    for (let i = 0; i <= 76; i++){
         let newNum = document.createElement("div")
+        if (i !== 0)
         newNum.innerText = i 
         newNum.classList.add("numbers")
         numbersContainer.appendChild(newNum)
     }
 }
+
+const randomNum = function (newRandom) {
+    const changeNum = document.getElementsByTagName("h1")[0];
+    changeNum.innerText =newRandom
+    const numberArray = document.querySelectorAll(".numbers")
+    numberArray[newRandom].classList.add("numbers1")
+  };
+  const btn1= document.querySelector("#random-num")
+  btn1.addEventListener('click', function(){
+    randomNum (Math.round(Math.random() * 76));
+       })
